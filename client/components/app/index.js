32a6bbf99a16e13a11661529ea925c564ce0fc78
Markus Bergh <hi@markusbergh.se>
2016-01-19 23:52:51 +0100

    Initial commit.

---
+++ client/components/app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {} from './style.scss';
+
+class App extends Component {
+    render() {
+        return <div>Hello World from a React component.</div>;
+    }
+}
+
+export default App;
